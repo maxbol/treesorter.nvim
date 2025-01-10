9d19ea7198e67a2c7951338d7a2b71fe8f421f5c
Max Bolotin <maks.bolotin@gmail.com>
2025-01-10 20:11:30 +0100

    Working textobjects integration, bug fixes

--- lua/treesorter/ts_utils.lua
+++ lua/treesorter/ts_utils.lua
@@ -1,43 +1,5 @@
 local M = {}
 
-M.node_name_types = {
-  "identifier",
-  "system_lib_string",
-  "string_literal",
-}
-
-M.node_name_fields = {
-  "name",
-  "declarator",
-  "path",
-  "field",
-}
-
-M.find_root_node = function(bufnr, node)
-  if not node then
-    node = vim.treesitter.get_node({ bufnr = bufnr })
-  end
-
-  local parent = node:parent()
-
-  if not parent then
-    return node
-  end
-
-  return M.find_root_node(bufnr, parent)
-end
-
-M.find_smallest_node_for_range = function(bufnr, range)
-  local last_line = vim.api.nvim_buf_get_lines(bufnr or 0, range[2], range[2] + 1, true)
-  if #last_line == 0 then
-    error("Invalid range")
-  end
-  local end_col = #last_line[1]
-  return vim.treesitter
-    .get_parser(bufnr)
-    :named_node_for_range({ range[1], 0, range[2], end_col }, { ignore_injections = false })
-end
-
 local function find_smallest_node_containing_oneof(needle, haystack)
   local parent = needle:parent()
 
@@ -52,6 +14,18 @@ local function find_smallest_node_containing_oneof(needle, haystack)
   end
 
   return find_smallest_node_containing_oneof(parent, haystack)
+end
+
+M.compose_iterators = function(iterators)
+  return function()
+    for _, iter in ipairs(iterators) do
+      local value = iter()
+      if value then
+        return value
+      end
+    end
+    return nil
+  end
 end
 
 M.find_container_from_query = function(query_iter, floor_node)
@@ -83,148 +57,67 @@ M.find_container_from_query = function(query_iter, floor_node)
   return find_smallest_node_containing_oneof(floor_node, captured_nodes)
 end
 
-M.find_nearest_ancestor_containing = function(type_filter, bufnr, node)
-  local child_iter = type_filter(node:iter_children())
-  local first_child = child_iter()
-
-  if first_child then
-    return node
+M.find_root_node = function(bufnr, node)
+  if not node then
+    node = vim.treesitter.get_node({
+      bufnr = bufnr,
+      ignore_injections = false,
+    })
   end
-
   local parent = node:parent()
 
   if not parent then
-    return nil
+    return node
   end
 
-  return M.find_nearest_ancestor_containing(type_filter, bufnr, parent)
+  return M.find_root_node(bufnr, parent)
 end
 
-M.oneoff_iterator = function(value)
-  local called = false
-  return function()
-    if called then
-      return nil
+M.find_smallest_node_for_range = function(bufnr, range)
+  local last_line = vim.api.nvim_buf_get_lines(bufnr or 0, range[2], range[2] + 1, true)
+  if #last_line == 0 then
+    error("Invalid range")
+  end
+  local end_col = #last_line[1]
+  return vim.treesitter
+    .get_parser(bufnr)
+    :named_node_for_range({ range[1], 0, range[2], end_col }, { ignore_injections = false })
+end
+
+M.get_captures = function(bufnr, range)
+  local node
+  if range then
+    node = M.find_smallest_node_for_range(bufnr, range)
+  else
+    node = M.find_root_node(bufnr)
+  end
+
+  local captures_set = {}
+  local textobjects_query = M.get_textobjects_query(bufnr)
+
+  if not textobjects_query then
+    return {}
+  end
+
+  local query_it = textobjects_query:iter_captures(node, bufnr, 0)
+
+  while true do
+    local id = query_it()
+    if not id then
+      break
     end
-    called = true
-    return value
-  end
-end
-
-M.get_parent_filter = function(parent)
-  return function(iter)
-    return function()
-      while true do
-        local node = iter()
-        if not node then
-          return nil
-        end
-
-        if node:parent():equal(parent) then
-          return node
-        end
-      end
-    end
-  end
-end
-
-M.get_type_filter = function(types)
-  return function(iter)
-    return function()
-      while true do
-        local child = iter()
-        if not child then
-          return nil
-        end
-
-        if vim.tbl_contains(types, child:type()) then
-          return child
-        end
-      end
+    local name = textobjects_query.captures[id]
+    if name ~= nil and name:find("_") ~= 1 then
+      captures_set["@" .. name] = true
     end
   end
-end
 
-M.get_range_filter = function(range)
-  return function(iter)
-    return function()
-      while true do
-        local child = iter()
-        if not child then
-          return nil
-        end
-
-        local start_row = child:start()
-        local end_row = child:end_()
-
-        if start_row >= range[1] and end_row <= range[2] then
-          return child
-        end
-      end
-    end
-  end
-end
-
-M.get_query_node_iterator = function(iter)
-  return function()
-    local capture = { iter() }
-    if not capture then
-      return nil
-    end
-
-    return capture[2]
-  end
-end
-
-M.compose_iterators = function(iterators)
-  return function()
-    for _, iter in ipairs(iterators) do
-      local value = iter()
-      if value then
-        return value
-      end
-    end
-    return nil
-  end
-end
-
-M.get_first_child_by_type = function(node, child_type)
-  if not node then
-    return nil
+  local captures = {}
+  for k, _ in pairs(captures_set) do
+    captures[#captures + 1] = k
   end
 
-  local iter = M.get_type_filter({ child_type })(node:iter_children())
-  return iter()
-end
-
-M.node_to_lines = function(node)
-  if not node then
-    return nil
-  end
-
-  local start_row = node:start()
-  local end_row = node:end_()
-  return vim.api.nvim_buf_get_lines(0, start_row, end_row, false)
-end
-
-M.node_to_text = function(node)
-  if not node then
-    return nil
-  end
-
-  local start_row, start_col = node:start()
-  local end_row, end_col = node:end_()
-  return vim.api.nvim_buf_get_text(0, start_row, start_col, end_row, end_col, {})
-end
-
-M.node_to_string = function(node)
-  local text = M.node_to_text(node)
-
-  if not text then
-    return nil
-  end
-
-  return table.concat(text, "\n")
+  return captures
 end
 
 M.get_node_name = function(node)
@@ -257,6 +150,87 @@ M.get_node_name = function(node)
   error("Node type not supported: " .. node:type())
 end
 
+M.get_parent_filter = function(parent)
+  return function(iter)
+    return function()
+      while true do
+        local node = iter()
+        if not node then
+          return nil
+        end
+
+        if node:parent():equal(parent) then
+          return node
+        end
+      end
+    end
+  end
+end
+
+M.get_query_node_iterator = function(iter)
+  return function()
+    local capture = { iter() }
+    if not capture then
+      return nil
+    end
+
+    return capture[2]
+  end
+end
+
+M.get_range_filter = function(range)
+  return function(iter)
+    return function()
+      while true do
+        local child = iter()
+        if not child then
+          return nil
+        end
+
+        local start_row = child:start()
+        local end_row = child:end_()
+
+        if start_row >= range[1] and end_row <= range[2] then
+          return child
+        end
+      end
+    end
+  end
+end
+
+M.get_textobjects_query = function(bufnr)
+  local filetype = vim.api.nvim_get_option_value("filetype", {
+    buf = bufnr,
+  })
+
+  local ok, textobjects_query = pcall(vim.treesitter.query.get, filetype, "textobjects")
+
+  if ok == false then
+    textobjects_query = nil
+  end
+
+  return textobjects_query
+end
+
+M.get_types = function(bufnr, range)
+  local node
+  if range then
+    node = M.find_smallest_node_for_range(bufnr, range)
+  else
+    node = M.find_root_node(bufnr)
+  end
+  local typeset = M.get_typeset(node)
+  local types = {}
+
+  for type, _ in pairs(typeset) do
+    table.insert(types, type)
+  end
+
+  table.sort(types)
+
+  return types
+end
+
 M.get_typeset = function(node, typeset)
   if not typeset then
     typeset = {}
@@ -281,24 +255,12 @@ M.get_typeset = function(node, typeset)
   return typeset
 end
 
-M.get_types = function(bufnr, range)
-  local node
-  if range then
-    node = M.find_smallest_node_for_range(bufnr, range)
-  else
-    node = M.find_root_node(bufnr)
-  end
-  local typeset = M.get_typeset(node)
-  local types = {}
-
-  for type, _ in pairs(typeset) do
-    table.insert(types, type)
-  end
-
-  table.sort(types)
-
-  return types
-end
+M.node_name_fields = {
+  "name",
+  "declarator",
+  "path",
+  "field",
+}
 
 M.node_name_types = {
   "identifier",
@@ -307,5 +269,38 @@ M.node_name_types = {
   "system_lib_string",
   "string_literal",
 }
+
+M.node_name_types = {
+  "identifier",
+  "system_lib_string",
+  "string_literal",
+}
+
+M.node_to_string = function(node)
+  local text = M.node_to_text(node)
+
+  if not text then
+    return nil
+  end
+
+  return table.concat(text, "\n")
+end
+
+M.node_to_text = function(node)
+  if not node then
+    return nil
+  end
+
+  local start_row, start_col = node:start()
+  local end_row, end_col = node:end_()
+  return vim.api.nvim_buf_get_text(0, start_row, start_col, end_row, end_col, {})
+end
+
+M.unpack_range_end_exlusive = function(range)
+  if not range then
+    return nil, nil
+  end
+  return range[1], range[2] + 1
+end
 
 return M
